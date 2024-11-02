41383457e6fbc33b40dd4358676d42bda1293334
Sarmad682 <waheedsarmad682@gmail.com>
2024-11-02 18:52:56 +0500

    First

---
+++ Portfolio/main.js
@@ -0,0 +1,22 @@
+
+// typed( ".text" , {
+//     strings:["Frontend Developer" , "Youtuber" , "Web Developer"],
+//     typeSpeed: 100,
+//     backSpeed: 100,
+//     backdelay: 1000,    
+//     loop: true
+// })
+
+// var typed = new Typed('.text', {
+//     strings: ['<i>First</i> sentence.', '&amp; a second sentence.'],
+//     typeSpeed: 50,
+//   });
+
+
+var animation = new Typed('.text' , {
+    strings :['Flutter Developer' , 'Web Developer' , 'ABAP Developer'],
+    typeSpeed: 50,
+    backSpeed: 100,
+    backdelay: 1000,    
+    loop: true
+}); 
